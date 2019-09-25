7106164bec6136df0a6f45cc239c8f8842943c0c
chue zhang <czhang003@citymail.cuny.edu>
2019-09-25 19:20:04 -0400

    Tkinter stuff

--- Tkinter.py
+++ Tkinter.py
@@ -1,1 +1,16 @@
-print('hello')
+from tkinter import *
+
+window = Tk()
+window.geometry("300x300")  # for frame sizing
+window.title("Welcome")
+
+
+def exit1():
+    exit();
+
+
+button1 = Button(window, text="burger", relief=RAISED, command=exit1)  # GROOVE, RIDGE, SUNKEN, RAISED
+# command = "function" to give button functionality
+button1.place(x=60, y=50)
+
+window.mainloop()
