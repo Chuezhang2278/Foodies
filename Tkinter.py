326b588d4ffcbdbc928e0e00b1d2d38a85fd80a4
chue zhang <czhang003@citymail.cuny.edu>
2019-09-25 21:41:56 -0400

    PyQt5

--- Tkinter.py
+++ Tkinter.py
@@ -1,16 +1,5 @@
-from tkinter import *
-
-window = Tk()
-window.geometry("300x300")  # for frame sizing
-window.title("Welcome")
-
-
-def exit1():
-    exit();
-
-
-button1 = Button(window, text="burger", relief=RAISED, command=exit1)  # GROOVE, RIDGE, SUNKEN, RAISED
-# command = "function" to give button functionality
-button1.place(x=60, y=50)
-
-window.mainloop()
+from PyQt5.QtWidgets import QApplication, QLabel
+app = QApplication([])
+label = QLabel('Hi')
+label.show()
+app.exec_()
