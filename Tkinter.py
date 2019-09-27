46b8895f58a7894acd6c182115d3dcc796cb0d7f
chue zhang <czhang003@citymail.cuny.edu>
2019-09-27 18:29:52 -0400

    Event handeling test

--- Tkinter.py
+++ Tkinter.py
@@ -8,9 +8,11 @@ class Window(QDialog):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.ui.buttonBox.clicked.connect(self.disp) # Event handeling when button is clicked, self.disp is calling function below
         self.show()
+    def disp(self): # Function activated when button is pressed
+        print('hello') # Action
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     w = Window()
-    w.show()
     sys.exit(app.exec_())
