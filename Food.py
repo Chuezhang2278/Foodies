1f5b5b0364b548604bc8eb38e8d85a58e94f8a79
chue zhang <czhang003@citymail.cuny.edu>
2019-11-19 12:02:27 -0500

    small bug fix

--- Food.py
+++ Food.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Food_Window()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
