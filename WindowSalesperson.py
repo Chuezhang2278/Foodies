acec1fdd70c4129aa1b8d824a8781bbd5aad06fc
chue zhang <czhang003@citymail.cuny.edu>
2019-11-26 12:44:43 -0500

    Name Change for simplicity

--- WindowSalesperson.py
+++ WindowSalesperson.py
@@ -8,7 +8,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_MainWindow(object):
+class Sales_Window(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 697)
@@ -1005,7 +1005,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Sales_Window()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
