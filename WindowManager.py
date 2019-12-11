db6e30bd94c2b2a9a83a0b203ff4ea812502801a
JiandeLi1 <47312442+JiandeLi1@users.noreply.github.com>
2019-12-11 10:23:04 -0500

    Add files via upload

--- WindowManager.py
+++ WindowManager.py
@@ -13,9 +13,12 @@ from WindowStaffManagement import Ui_Form_StaffManagement
 from CusInforManagement import Ui_Form_CusInfor
 from WindowComplaintManagement import Ui_Form_ComplainManagement
 from WindowEmployeeSignUp import Ui_Form_EmployeeSignUp
+#from WindowLogin import LoginWindow
+
 
 
 class  ManagerWindow(object):
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(604, 493)
@@ -27,7 +30,7 @@ class  ManagerWindow(object):
         self.label.setGeometry(QtCore.QRect(10, 10, 201, 41))
         self.label.setObjectName("label")
         self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(92, 120, 291, 181))
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(92, 120, 291, 191))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
@@ -58,6 +61,9 @@ class  ManagerWindow(object):
         self.exitbutton = QtWidgets.QPushButton(self.centralwidget)
         self.exitbutton.setGeometry(QtCore.QRect(455, 350, 91, 31))
         self.exitbutton.setObjectName("exitbutton")
+        #self.backbutton = QtWidgets.QPushButton(self.centralwidget)
+        #self.backbutton.setGeometry(QtCore.QRect(355, 350, 91, 31))
+        #self.backbutton.setObjectName("backbutton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 604, 18))
@@ -76,6 +82,7 @@ class  ManagerWindow(object):
         self.HandlesComplaintsB.clicked.connect(self.ComplainManafementstr)
         self.ESignB.clicked.connect(self.Esignstr)
         self.exitbutton.clicked.connect(lambda: self.closescr(MainWindow))
+        #self.backbutton.clicked.connect(self.switch_login)
 
     def DecidesCommissionsscr(self):
         self.Form = QtWidgets.QWidget()
@@ -116,6 +123,12 @@ class  ManagerWindow(object):
     def closescr(self, MainWindow):
         MainWindow.hide()
 
+    # def switch_login(self):
+    #     self.Form = QtWidgets.QWidget()
+    #     self.ui = LoginWindow
+    #     self.ui.setupUi(self.Form)
+    #     self.Form.show()
+    #     self.closescr()
 
 
     def retranslateUi(self, MainWindow):
@@ -129,6 +142,7 @@ class  ManagerWindow(object):
         self.CustomerInforB.setText(_translate("MainWindow", "Customer Information Mangement"))
         self.ESignB.setText(_translate("MainWindow", "Empoloyee Signup"))
         self.exitbutton.setText(_translate("MainWindow", "Exit"))
+        #self.backbutton.setText(_translate("MainWindow", "Back"))
 
 if __name__ == "__main__":
     import sys
